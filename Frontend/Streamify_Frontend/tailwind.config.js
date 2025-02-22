404880caca8fcc1576de82034f4aa20ba1fc9d06
Pratyush6653 <Pratyush0231@gmail.com>
2025-02-22 16:44:34 +0530

    changes due to tailwind version

--- Frontend/Streamify_Frontend/tailwind.config.js
+++ Frontend/Streamify_Frontend/tailwind.config.js
@@ -1,10 +1,11 @@
-module.exports = {
-    content: [
-      "./src/**/*.{html,js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
