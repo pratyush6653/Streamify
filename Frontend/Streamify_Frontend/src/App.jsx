36d174837b3a7860f7bca71c8d927d7b14c8f87d
Pratyush6653 <Pratyush0231@gmail.com>
2025-02-23 11:27:16 +0530

    add padding

--- Frontend/Streamify_Frontend/src/App.jsx
+++ Frontend/Streamify_Frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <div className="flex flex-col items-center justify-center min-h-screen">
         {/* Centered Heading */}
-        <h1 className="text-5xl font-extrabold text-center mb-8 bg-gradient-to-r from-[#30cfd0] to-[#330867] bg-clip-text text-transparent animate-gradient">
+        <h1 className="text-5xl font-extrabold text-center mb-8 bg-gradient-to-r from-[#30cfd0] to-[#330867] bg-clip-text text-transparent animate-gradient py-2">
         Welcome To Streamify
         </h1>
         {/* Video Upload Component */}
